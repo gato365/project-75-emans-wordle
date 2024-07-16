b51b598de2747757dc049e76079adc75d19009e4
gato365 <immanuelwilliams@gato365.com>
2024-07-16 04:22:47 -0700

    Able to get routes to work but website css and js files are not loading

--- wordle_gato365/users/views.py
+++ wordle_gato365/users/views.py
@@ -44,7 +44,7 @@
 
 
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('<h1>Wordle Home</h1>')
+    return render(request, 'users/base.html')
--- wordle_gato365/wordle_gato365/urls.py
+++ wordle_gato365/wordle_gato365/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
+    path('', user_views.home),
     path('admin/', admin.site.urls),
-    path('home', include('users.urls'))
+    
+    path('home/', user_views.home)
 
 #     path('register/', ),
 #     path('login/', name='login'),
