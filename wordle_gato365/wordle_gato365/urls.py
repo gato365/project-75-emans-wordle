9260dc9bf3925289cebc94270d50a27c670a07a0
gato365 <immanuelwilliams@gato365.com>
2024-07-16 09:03:03 -0700

    Made routes work but not the logic

--- wordle_gato365/wordle_gato365/urls.py
+++ wordle_gato365/wordle_gato365/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', user_views.home),
@@ -24,8 +25,8 @@ urlpatterns = [
     
     path('home/', user_views.home),
     path('profile/', user_views.profile),
-#     path('register/', ),
-#     path('login/', name='login'),
+    path('register/', user_views.register),
+    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
 #     path('logout/', ),
 #     path('', include('user.urls')),
 #  path('profile/', name='profile'),   
