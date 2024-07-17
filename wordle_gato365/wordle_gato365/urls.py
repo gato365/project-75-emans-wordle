391be1301a8e834d578ceaa6371af554cdb465d0
gato365 <immanuelwilliams@gato365.com>
2024-07-17 16:23:20 -0700

    chore: Update user views to include ProfileUpdateForm, able to login and register views

--- wordle_gato365/wordle_gato365/urls.py
+++ wordle_gato365/wordle_gato365/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('home/', user_views.home),
-    path('profile/', user_views.profile),
+    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
-    # path('logout/', ),
+#     path('logout/', ),
 #     path('', include('user.urls')),
-#  path('profile/', name='profile'),   
+   
 ]
