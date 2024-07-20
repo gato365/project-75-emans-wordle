1e35b5edc0cf901cfd88d633dd11248790f4a57c
gato365 <immanuelwilliams@gato365.com>
2024-07-20 07:35:00 -0700

    chore: Update CSS styles for Wordle game and remove redundant styles

--- wordle_gato365/wordle/static/wordle/js/scripts.js
+++ wordle_gato365/wordle/static/wordle/js/scripts.js
@@ -1,7 +1,7 @@
 let answer;
 // Fetch the text file located one directory up
 
-fetch('../five_letter_words.txt')
+fetch('five_letter_words.txt')
   .then(response => response.text()) // Convert the response to text
   .then(text => {
     const words = text.split('\n'); // Split the text into an array of words
