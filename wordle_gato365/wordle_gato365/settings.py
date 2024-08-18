977788a4e841dcf344336bc96922e701f95bed48
gato365 <immanuelwilliams@gato365.com>
2024-08-18 10:56:10 -0700

    Addd static

--- wordle_gato365/wordle_gato365/settings.py
+++ wordle_gato365/wordle_gato365/settings.py
@@ -150,6 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'wordle','static')
+
  # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
