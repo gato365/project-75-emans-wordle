1896baa8f60e99e9e7d83c87133e688db9cf2273
gato365 <immanuelwilliams@gato365.com>
2024-09-06 12:37:31 -0700

    tryign new

--- wordle_gato365/wordle_gato365/wsgi.py
+++ wordle_gato365/wordle_gato365/wsgi.py
@@ -8,24 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-print("Current Python sys.path:", sys.path)
-## Begin 1: Logging Information
-import logging 
-logger = logging.getLogger(__name__)
-## End 1: Logging Information
-
-## Begin 2: Logging Information
-logger.debug("Setting DJANGO_SETTINGS_MODULE")
-## End 2: Logging Information
 
 
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordle_gato365.settings')
 
-## Begin 3: Logging Information
-logger.debug("Getting WSGI application")
-## End 3: Logging Information
 
 application = get_wsgi_application()
 
