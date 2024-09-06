c030f51664154a3a3da8835048078f38ff013b08
gato365 <immanuelwilliams@gato365.com>
2024-09-06 06:29:53 -0700

    Refactor wsgi.py to fix settings directory and add logging features

--- wordle_gato365/wordle_gato365/wsgi.py
+++ wordle_gato365/wordle_gato365/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+print("Current Python sys.path:", sys.path)
 ## Begin 1: Logging Information
 import logging 
 logger = logging.getLogger(__name__)
@@ -20,7 +21,7 @@ logger.debug("Setting DJANGO_SETTINGS_MODULE")
 
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordle_gato365.wordle_gato365.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordle_gato365.settings')
 
 ## Begin 3: Logging Information
 logger.debug("Getting WSGI application")
