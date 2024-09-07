6d6ddccd92c8d413ecc021a17b25f9af3575d8cd
gato365 <immanuelwilliams@gato365.com>
2024-09-07 09:30:53 -0700

    fix33-domain name

--- wordle_gato365/wordle_gato365/settings.py
+++ wordle_gato365/wordle_gato365/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-6g9e3!e_&!vif@5i^hktv^+lq-y&-=xv-^brjcakq8-f-if4qu
 ## 1b) Currently in use
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wordle-app-2024-4c250a9e97a3.herokuapp.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
