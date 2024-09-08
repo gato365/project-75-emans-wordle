fd5e960f778e2b7877dcab9e708cad8a5c984a0f
gato365 <immanuelwilliams@gato365.com>
2024-09-08 06:01:37 -0700

    fix34-Update default_port in runserver command to use environment variable 'PORT' if available, fallback to 8000 or 80 based on the environment

--- wordle_gato365/manage.py
+++ wordle_gato365/manage.py
@@ -3,17 +3,22 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordle_gato365.settings')
 
-
+    # Determine if running locally or remotely
+    environment = os.getenv('ENVIRONMENT', 'local')
 
     try:
         from django.core.management import execute_from_command_line
         from django.core.management.commands.runserver import Command as runserver
-        runserver.default_port = os.environ.get('PORT',8000)
+
+        if environment == 'local':
+            runserver.default_port = os.environ.get('PORT', '8000')
+        else:
+            runserver.default_port = os.environ.get('PORT', '80')  # Default port for remote
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -21,7 +26,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
