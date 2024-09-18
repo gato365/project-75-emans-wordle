2220069a7ff94b1b97ebe0ba8dab4ff131c8ae22
gato365 <immanuelwilliams@gato365.com>
2024-09-18 12:16:34 -0700

    Enhance user registration form; update email label and major field prompt; set default graduating class; improve message styling in CSS

--- wordle_gato365/users/forms.py
+++ wordle_gato365/users/forms.py
@@ -9,19 +9,23 @@ CustomUser = get_user_model()
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(label="Enter your CalPoly email")
     user_type = forms.ChoiceField(choices=CustomUser.USER_TYPE_CHOICES)
-    graduating_class = forms.ChoiceField(choices=CustomUser.GRADUATING_CLASS_CHOICES, required=False)
+    graduating_class = forms.ChoiceField(choices=CustomUser.GRADUATING_CLASS_CHOICES)
     college = forms.ChoiceField(choices=CustomUser.COLLEGE_CHOICES)
-    major = forms.CharField(max_length=100, required=False)
+    major = forms.CharField(label = 'Enter Major or "NA" if not applicable:', max_length=100)
     
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2', 'user_type', 'graduating_class', 'college', 'major']
+        labels = {
+            'username': 'Enter your Cal Poly username',
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['graduating_class'].widget = forms.Select(choices=CustomUser.GRADUATING_CLASS_CHOICES)
+        # self.fields['graduating_class'].widget = forms.Select(choices=CustomUser.GRADUATING_CLASS_CHOICES)
+        self.fields['graduating_class'].initial = '2027'
         self.fields['graduating_class'].widget.attrs['class'] = 'student-field'
         self.fields['major'].widget.attrs['class'] = 'student-field'
 
