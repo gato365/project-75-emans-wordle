0cd0f4b71e7146fe721de5977e6005f05ab25d2a
gato365 <immanuelwilliams@gato365.com>
2024-10-27 09:02:06 -0700

    Add tournament list view with active tournaments filter

--- wordle_gato365/tournament/views.py
+++ wordle_gato365/tournament/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Tournament, TournamentTeam
 
-# Create your views here.
+class TournamentListView(LoginRequiredMixin, ListView):
+    model = Tournament
+    template_name = 'tournament/tournament_list.html'
+    context_object_name = 'tournaments'
+    
+    def get_queryset(self):
+        return Tournament.objects.filter(is_active=True)
+
+# Add other views as needed...
